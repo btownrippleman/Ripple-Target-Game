500854ea3ca93a33bfd9947df01bae61c977aae1
root <bradenripple@gmail.com>
2015-05-05 02:43:06 -0700

    this isn't extra credit but it is working now

--- main.lua
+++ main.lua
@@ -16,6 +16,8 @@ local rect
 local weapon = display.newImageRect( "guitar.png", 100, 50 )
 --local bullet
 local explosion
+local targetHit
+local shotFired = false
 local hits=0
 local misses=0
 local scoreText = display.newText("hits:0 misses:0 %hits :0", 160,150,"",18)
@@ -41,11 +43,10 @@ local function rectArrayMaker(numOfCopies,distance,centerX,centerY,spacePercenta
 end
 
 function targetMake() -- this is called every time a target is destroyed either by a bullet or going off screen
+  shotFired = false
   local rect = display.newRect(0,0,45,45) -- I initially used this code for a rotating ear, but I used a rect instead
-  rect:setFillColor(0,0,1)
+  rect:setFillColor(0,0,1) -- make the rect color blue
   target = display.newGroup()  -- ear = display.newImageRect("ear.png",45,45)
-  target.shotAt = false -- this is used denote whether the target has been shot and made true on bulletFire
-  target.speed =4
   target.x = 0 --display.contentCenterX
   target.y =  math.random(HEIGHT)
   target:insert(rect)
@@ -53,12 +54,11 @@ function targetMake() -- this is called every time a target is destroyed either 
   target:insert(targetSurround)
   targetSurround.x = 0 - target.x
   targetSurround.y = 0 - target.y
-  --target.x = target.x +10
   target.xScale = .6
   target.yScale = .6
   target.radius = 45*.6
   local transitionTime = 1000
-  if hits~= 0 and misses~= 0 then transitionTime = 500 +math.random()*1000*misses/hits
+  if hits~= 0 and misses~= 0 then transitionTime = 500 +math.random()*1000*(misses+hits)/hits
   else transitionTime = 1500 end
   transition.to(target, {time = transitionTime , rotation =360*(WIDTH/(2*math.pi*45)), alpha = 1, x = WIDTH,  onComplete =  destroyTarget })
  end
@@ -114,49 +114,47 @@ function destroyExplosion() --  would have used a generic destroyObject(obj) con
   explosion:removeSelf(); explosion = nil -- in transition.to(explosion, {...}) didn't like it
 end
 
+function destroyTarget()
+  if shotFired == false then misses = misses +1 end
+  target:removeSelf(); target = nil;
+end
 
 function destroyBullet() -- same goes for what is said above, but I try to call the destroy function only appropriate times
+  if targetHit == false then misses = misses +1 end
    if bullet~= nil then bullet:removeSelf(); bullet = nil;  end
 end
 
 function bulletFire(event,weapon) -- called by myTouchListener from the tappings on the background.
+    shotFired = true;
+    targetHit = false -- if this value does not change during the life the bullet, it will register as a miss.
     local u = unitVector(event,weapon); local length = vectorLength(event,weapon)
     local localbullet = display.newImageRect("note.png", 44, 44) --1.2*length*u[1], 1.2*length*u[2])
     localbullet.x = weapon.x + 100*u[1]; localbullet.y = weapon.y + 100*u[2] -- bullets will spawn at about 100 units from weapons base
     localbullet.rotation = objectRotation(event,weapon)+90
-    transition.to(localbullet, {time = 150, alpha = 1,x = event.x,y = event.y,  onComplete = bulletMiss })
-     -- bring the text to the fron tof the screen
-    -- I would have used transition to but I didn't like how the object stopped at the top of the screen'
+    transition.to(localbullet, {time = 150, alpha = 1,x = event.x,y = event.y,  onComplete = destroyBullet })
 
     return localbullet -- returns a value as req'd in the module
 end
-local function scoreTextRenew() --  call this at every frame of newFrame to make sure the text is in the Front.
-    scoreText.text = "hits: " .. hits .. " misses: " .. misses .. " %hits :"..math.round(100*hits/(hits+misses))
-    scoreText:toFront()
-end
-
-function destroyTarget()
-  if target.shotAt == false then
-     misses = misses +1 end
-  target:removeSelf(); target = nil;
-end
-
-function bulletMiss() -- if a bullet
-  if bullet ~= nil and target~= nil then target.shotAt = true end
-  misses = misses +1; destroyBullet();
-end
-
-
 
 function explosionMake(x,y)  -- used easing.outExpo, increased the fastest, i.e. being an exponential function.
   local rand = math.ceil(math.random(7))
   local explosion = display.newImageRect("boom"..rand..".png",22,22);
   explosion.x = x;
   explosion.y = y;
-  local explSize = 15+ math.random()*math.random()*math.random()*25
+  local explSize = 15
+  if hits ~= 0 or misses ~= 0 then explSize = math.random() *30*hits/(hits+misses) end
   transition.to(explosion,{ time=1000, xScale=explSize, yScale = explSize, alpha = 0, transition=easing.outExpo })
 
 end
+
+local function scoreTextRenew() --  call this at every frame of newFrame to make sure the text is in the Front.
+    if misses ~= 0 or hits ~= 0 then scoreText.text = "hits: " .. hits .. " misses: " .. misses .. " %hits :"..math.round(100*hits/(hits+misses)) end
+      scoreText:toFront()
+end
+
+
+
+
 
 local function withinRange(range, obj1, obj2) -- used to find if objs are in range return a false or true
 
@@ -177,13 +175,14 @@ local function newFrame() --this function corresponds to the eventListener for R
        if bullet ~= nil and target ~= nil then
           if withinRange(55,bullet,target)
           then
+            targetHit = true;
+            hits = hits+1;
             explosionMake(target.x,target.y);
             transition.cancel(target,bullet,explosion);
             destroyBullet();
-            target.shotAt = true;
             destroyTarget();
-            hits = hits+1;
-          end
+
+           end
        end
 
 
